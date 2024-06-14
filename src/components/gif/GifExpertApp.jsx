8d1f3423f583a442f2e8e19cb24ecd825527527c
Fabio Cedres <fcedres19@gmail.com>
2024-06-13 22:13:41 -0300

    Sacamos titulo gif expert

--- src/components/gif/GifExpertApp.jsx
+++ src/components/gif/GifExpertApp.jsx
@@ -16,7 +16,7 @@ export const GifExpertApp = ({onImageSelect}) => {
 
     return (
         <>
-            <h1>GifExpertApp</h1>
+            
 
             <AddCategory 
                 //setCategories={ setCategories }
